95512b5d9773a070f876986f0a12c04893e7689f
Yumeto Yamagishi <yumeto-yamagishi@mamezou.com>
2022-08-09 13:45:19 +0900

    Add TODO comment

--- src/ops-handlers/index.ts
+++ src/ops-handlers/index.ts
@@ -2,6 +2,12 @@ import { handlerPath } from "../utils/handler-resolver"
 
 const handleApiGatewayLog = {
   handler: `${handlerPath(__dirname)}/cloudwatch-logs-handler.main`,
+
+  // TODO Fix
+  // Warning: Invalid configuration encountered
+  // at 'functions.handleApiGatewayLog.events.0': unsupported function event
+  //
+  // Learn more about configuration validation here: http://slss.io/configuration-validation
   events: [
     {
       subscriptionFilter: {
