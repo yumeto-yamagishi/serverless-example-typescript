2571bad83c5433258c6ce21a4fc22737f71b87ed
Yumeto Yamagishi <yumeto-yamagishi@mamezou.com>
2022-08-09 10:24:27 +0900

    Uninstall validate.js

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,22 +1,5 @@
-import validate from "validate.js/validate";
-
-import ResponseModel from "../models/response.model";
-
 export type Constraints = { [key in string]: unknown };
 
-export const validateRequest = async <INPUT>(
-  values: INPUT,
-  constraints: {body?: Constraints, query?: Constraints}
-): Promise<INPUT> => {
-  const validation = {
-    ...(constraints.body && validate(values["body"], constraints.body) || {}),
-    ...(constraints.query && validate(values["queryStringParameters"], constraints.query) || {}),
-  };
-    if (Object.keys(validation).length === 0) {
-      return values;
-    }
-    throw new ResponseModel({ validation }, 400, "required fields are missing");
-};
 
 export const createChunks = <T>(data: T[], chunkSize: number): T[][] => {
   const urlChunks: T[][] = [];
